d0de297c1c78fdae68c4451811684883631b516f
Kjell Breiland <kjell.breiland@gmail.com>
2018-02-05 14:32:04 -0800

    remove old un used compression function

--- compress.go
+++ compress.go
@@ -1,12 +1,5 @@
 package poslog
 
-import (
-	"archive/zip"
-	"fmt"
-	"path/filepath"
-	"strings"
-)
-
 // POSLogs is an array of poslog grouped by store(s) and dayid(s)
 type POSLogs struct {
 	DayIDs  []string
@@ -34,42 +27,4 @@ func (ps *POSLogs) appendStore(s int) {
 		}
 		return
 	}
-}
-
-func (ps *POSLogs) appendPOSLog(p POSLog) {
-	ps.POSLogs = append(ps.POSLogs, p)
-	return
-}
-
-// ZipReadAllXML Reads all XML from a passed archive
-func ZipReadAllXML(archive string) (ps POSLogs) {
-
-	an := strings.TrimSuffix(filepath.Base(archive), filepath.Ext(filepath.Base(archive)))
-	reader, err := zip.OpenReader(archive)
-	if err != nil {
-		fmt.Println("FUCK THE ZIP DIDN'T WORK", archive)
-	}
-
-	for _, file := range reader.File {
-		ext := filepath.Ext(file.Name)
-		noext := strings.TrimSuffix(file.Name, ext)
-		ofp := filepath.Join("output", an)
-
-		if ext == ".xml" {
-
-			ofn := filepath.Join(ofp, noext)
-			ofn = "./" + ofn
-			fileReader, err := file.Open()
-			if err != nil {
-				fmt.Println("ERRUR")
-			}
-			defer fileReader.Close()
-
-			p := importReaderXML(fileReader, file.Name)
-
-			ps.appendPOSLog(p)
-		}
-	}
-
-	return
 }
