bc644cb89f9e0fb2c49033a9ec6616e8bd9b1ddc
Kjell Breiland <kjell.breiland@gmail.com>
2018-02-05 15:13:27 -0800

    comment out test until i want it in there

--- type_test.go
+++ type_test.go
@@ -1,10 +1,6 @@
 package poslog
 
-import (
-	"testing"
-)
-
-func TestReadPOSLog(t *testing.T) {
-	filename := "./input/xml/POSLog-201612301530-54.xml"
-	p := ImportXML(filename)
-}
+// func TestReadPOSLog(t *testing.T) {
+// 	filename := "./input/xml/POSLog-201612301530-54.xml"
+// 	p := ReadXML(filename)
+// }
