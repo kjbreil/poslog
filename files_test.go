605dbb80937a93b84c8b158069586127460813c8
Kjell Breiland <kjell.breiland@gmail.com>
2018-02-05 15:14:42 -0800

    test out changes

--- files_test.go
+++ files_test.go
@@ -6,11 +6,6 @@ import (
 
 func TestWriteXML(t *testing.T) {
 	// data := ImportXML("./input/xml/POSLog-201612301530-54.xml")
-	data := ImportXML("./input/xml/POSLog.xml")
+	data := ReadXML("./sample/POSLog.xml")
 	data.WriteXML("./output/TestOut.xml")
-}
-
-func TestWriteJSON(t *testing.T) {
-	data := ImportXML("./input/xml/POSLog-201612301530-54.xml")
-	WriteJSON("./output/TestOut", data)
 }
