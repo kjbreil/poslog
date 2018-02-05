ed494af6100f3d9c62931a15538818706ffc6f5a
Kjell Breiland <kjell.breiland@gmail.com>
2018-02-05 15:14:35 -0800

    adapt for strings from unmarshalling of time

--- methods.go
+++ methods.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"strconv"
 	"strings"
+	"time"
 )
 
 // filename is to append the filename to the poslog object
@@ -11,24 +12,11 @@ func (p *POSLog) filename(filename string) {
 	p.Filename = &filename
 }
 
-// dayid returns an int in dayid format YYYYMMDD
-// Should actuall Validate this as go time 20060102 like EndInt
-func (tr *Transaction) dayid() (dayid *int) {
-	format := "20061002"
-	mdy := tr.BusinessDayDate.Format(format)
-
-	did, err := strconv.Atoi(mdy)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return &did
-}
-
 // storeID returns a string of the store id for the poslog. This is not only informational but assures there is no major problem
 // with the poslog xml file
 func (p *POSLog) storeID() (storeID int) {
 	if len(p.Transaction) == 0 {
-		log.Println("No store id in ", p.Filename)
+		log.Println("No store id in ", &p.Filename)
 		storeID = 0
 		return
 	}
@@ -40,13 +28,59 @@ func (p *POSLog) storeID() (storeID int) {
 			log.Fatalln("Multiple Store ID's in single POSLog, I cannot handle that yet", t.RetailStoreID, storeID, p.Filename)
 		}
 	}
+	p.RetailStoreID = &storeID
 	return
+}
+
+// buisnessDayDate returns the buisness date of all transactions in a poslog file, all transactions in a poslog file should have SAME buisness date
+func (p *POSLog) buisnessDayDate() (buisnessDayDate *string) {
+	if len(p.Transaction) == 0 {
+		log.Println("No bid in ", p.Filename)
+		*buisnessDayDate = "0"
+		return
+	}
+
+	buisnessDayDate = &p.Transaction[0].BusinessDayDate
+
+	for _, tr := range p.Transaction {
+		if *buisnessDayDate != tr.BusinessDayDate {
+			log.Fatalln("Multiple Buisness days in single poslog file, I cannot handle that yet", tr.BusinessDayDate, *buisnessDayDate, *p.Filename)
+		}
+	}
+
+	p.BusinessDayDate = buisnessDayDate
+	return
+}
+
+// dayid returns an int in dayid format YYYYMMDD
+// Should actuall Validate this as go time 20060102 like EndInt
+func (tr *Transaction) dayid() (dayid *string) {
+
+	inputFormat := "2006-01-02"
+
+	time, err := time.Parse(inputFormat, tr.BusinessDayDate)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	format := "20060102"
+	mdy := time.Format(format)
+
+	// did, err := strconv.Atoi(mdy)
+	// if err != nil {
+	// 	log.Fatalln(err)
+	// }
+	return &mdy
 }
 
 // End returns an int of the transaction end datetime
 func (tr *Transaction) end() (endstring *string) {
+	time, err := time.Parse(time.RFC3339, tr.BusinessDayDate)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	format := "20060102150405"
-	*endstring = tr.EndDateTime.Format(format)
+	*endstring = time.Format(format)
 	return
 }
 
